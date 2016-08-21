0fb993fc4de4d090efbf3b243f0ec5272b23e20e
Olivia Campbell <ocampbell.network@gmail.com>
2016-08-21 17:00:11 +0100

    figaro added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [:show, :index, :new, :create] do
-    resources :doses, only: [:new, :create, :destroy]
+    resources :doses, only: [:new, :create]
   end
 
   resources :ingredients, only: [:show]
+  resources :doses, only: [:destroy]
 
   root "cocktails#index"
 end
